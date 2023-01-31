352ad0b786bec5769f707aa6bbe2b92bdef1c701
Robert Cabrera <cabrera.rch@gmail.com>
2023-01-31 17:55:25 +0100

    Change classes order

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <h1 className="font-bold underline text-red-500 text-sm">
+    <h1 className="text-sm font-bold text-red-500 underline">
       Esta es la Home
     </h1>
   );
