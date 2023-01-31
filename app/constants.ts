6abc891af94174655d9c335de5fdf75d8b07adb2
Robert Cabrera <cabrera.rch@gmail.com>
2023-01-31 18:22:19 +0100

    Take NavLink variable to constants file

---
+++ app/constants.ts
@@ -0,0 +1,12 @@
+import { NavLink } from "./interfaces/general";
+
+export const NAV_LINKS: NavLink[] = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "About",
+    route: "/about",
+  },
+];
