d2c1a0c762acdc5c5018fc4072f392aa234758d0
Robert Cabrera <cabrera.rch@gmail.com>
2023-02-04 12:32:04 +0100

    Update getMovies custom hook

--- hooks/useGetMovies.ts
+++ hooks/useGetMovies.ts
@@ -4,7 +4,7 @@ const fetcher = (url: any) => fetch(url).then((res) => res.json());
 export function useGetMovies() {
   const APP_MOVIES_API_KEY = process.env.NEXT_PUBLIC_APP_MOVIES_API_KEY;
 
-  const { data, error } = useSWR<any[]>(
+  const { data, error } = useSWR<any>(
     `https://api.themoviedb.org/3/movie/popular?api_key=${APP_MOVIES_API_KEY}`,
     fetcher
   );
