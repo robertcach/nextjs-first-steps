88ee8a9df87b381c127ca473066ebc9ec8853f7d
Robert Cabrera <cabrera.rch@gmail.com>
2023-02-08 18:42:47 +0100

    Change hook to use promises instead SWR to fetch data from server

--- hooks/useGetMovies.ts
+++ hooks/useGetMovies.ts
@@ -1,13 +1,7 @@
-import useSWR from "swr";
-const fetcher = (url: any) => fetch(url).then((res) => res.json());
-
 export function useGetMovies() {
   const APP_MOVIES_API_KEY = process.env.NEXT_PUBLIC_APP_MOVIES_API_KEY;
 
-  const { data, error } = useSWR<any>(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${APP_MOVIES_API_KEY}`,
-    fetcher
-  );
-
-  return { data, isLoading: !error && !data, isError: error };
+  return fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${APP_MOVIES_API_KEY}`
+  ).then((res) => res.json());
 }
