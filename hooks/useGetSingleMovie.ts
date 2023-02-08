5963982930b868d0dfdf04b6445687e1df2d9b0a
Robert Cabrera <cabrera.rch@gmail.com>
2023-02-08 18:44:04 +0100

    Change singleMovie hook to fetch data from server

--- hooks/useGetSingleMovie.ts
+++ hooks/useGetSingleMovie.ts
@@ -1,13 +1,7 @@
-import useSWR from "swr";
-const fetcher = (url: any) => fetch(url).then((res) => res.json());
-
 export function useGetSingleMovie(id: string) {
   const APP_MOVIES_API_KEY = process.env.NEXT_PUBLIC_APP_MOVIES_API_KEY;
 
-  const { data, error } = useSWR<any>(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${APP_MOVIES_API_KEY}&language=en-US`,
-    fetcher
-  );
-
-  return { data, isLoading: !error && !data, isError: error };
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${APP_MOVIES_API_KEY}&language=en-US`
+  ).then((res) => res.json());
 }
