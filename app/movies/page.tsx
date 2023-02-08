361ae60841c727fb6ce1fde155908cb44f961d8b
Robert Cabrera <cabrera.rch@gmail.com>
2023-02-08 18:43:40 +0100

    Update Movies page to get data from server

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,15 +1,14 @@
-"use client";
 import { useGetMovies } from "@/hooks/useGetMovies";
 import Link from "next/link";
 
-export default function Movies() {
-  const { data } = useGetMovies();
+export default async function Movies() {
+  const movies = await useGetMovies();
 
   return (
     <>
       <h1 className="text-xl font-bold ">Aquí mostramos las películas</h1>
       <ul>
-        {data?.results?.map((movie: any, index: number) => (
+        {movies?.results?.map((movie: any, index: number) => (
           <li key={index}>
             <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
               {movie.title}
