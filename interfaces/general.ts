5e06cae4352036a541d0c50b0875c970fd79fdf4
Robert Cabrera <cabrera.rch@gmail.com>
2023-02-11 12:18:50 +0100

    Create interfaces to type API response

--- interfaces/general.ts
+++ interfaces/general.ts
@@ -2,3 +2,25 @@ export interface NavLink {
   label: string;
   route: string;
 }
+
+export interface MovieResponse {
+  page: number;
+  results: Movie[];
+}
+
+export interface Movie {
+  poster_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
