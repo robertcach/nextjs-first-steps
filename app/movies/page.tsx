1db23f3131482a47776ae41925892055460d25be
Robert Cabrera <cabrera.rch@gmail.com>
2023-02-11 12:19:45 +0100

    Type fetch hook to get data properly

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,14 +1,16 @@
 import { useGetMovies } from "@/hooks/useGetMovies";
+import { MovieResponse } from "@/interfaces/general";
 import Link from "next/link";
 
 export default async function Movies() {
-  const movies = await useGetMovies();
+  const getMovies: MovieResponse = await useGetMovies();
+  const { results: movies } = getMovies;
 
   return (
     <>
       <h1 className="text-xl font-bold ">Aquí mostramos las películas</h1>
       <ul>
-        {movies?.results?.map((movie: any, index: number) => (
+        {movies?.map((movie: any, index: number) => (
           <li key={index}>
             <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
               {movie.title}
